95552ba6de65cfbd626e1d87eb6c5da7aba41ef5
PriyankaNigade18 <piyu1818@gmail.com>
2024-04-05 10:57:17 +0530

    Framework code for reference

---
+++ src/test/java/com/SwagLabs/TestCases/CheckOutPageTest.java
@@ -0,0 +1,21 @@
+package com.SwagLabs.TestCases;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class CheckOutPageTest extends BaseClass
+{
+  @Test(priority=1)
+  public void verifyCancel()
+  {
+	  ch.doCancel();
+	  addWait();
+	  cp.doCheckOut();
+  }
+  
+  @Test(priority=2)
+  public void verifyCheckout()
+  {
+	  ch.doContinue("Priyanka","Nigade","411047");
+  }
+}
